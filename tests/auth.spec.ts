f5b277ee5f14d0b5452d28eba9be0f323a3de841
opensprocket <234428359+jdbreiter@users.noreply.github.com>
2026-02-03 16:36:52 -0700

    add login test

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -15,3 +15,33 @@ test('registration successful', async ({ page }) => {
   await page.getByText('Bob', { exact: true }).click();
   await page.getByText('bob@bob.com').click();
 });
+
+test('login', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+  
+  await page.route('*/**/api/auth', async (route) => {
+    const loginReq = { email: 'd@jwt.com', password: 'a' };
+    const loginRes = {
+      user: {
+        id: 3,
+        name: 'Kai Chen',
+        email: 'd@jwt.com',
+        roles: [{ role: 'diner' }],
+      },
+      token: 'abcdef',
+    };
+    expect(route.request().method()).toBe('PUT');
+    expect(route.request().postDataJSON()).toMatchObject(loginReq);
+    await route.fulfill({ json: loginRes });
+  });
+  
+  
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('d@jwt.com');
+  await page.getByRole('textbox', { name: 'Email address' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('a');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'KC' }).click();
+  
+});
