5760b2752719b78ec5ed08744e71d515acffcade
opensprocket <opensprocket@users.noreply.github.com>
2026-02-22 22:46:54 -0700

    div soup, served fresh daily

--- src/views/adminDashboard.tsx
+++ src/views/adminDashboard.tsx
@@ -144,6 +144,91 @@ export default function AdminDashboard(props: Props) {
         <div>
           <Button className="w-36 text-xs sm:text-sm sm:w-64" title="Add Franchise" onPress={createFranchise} />
         </div>
+        <div className="text-start py-8 px-4 sm:px-6 lg:px-8">
+          <h3 className="text-neutral-100 text-xl">Users</h3>
+          <div className="bg-neutral-100 overflow-clip my-4">
+            <div className="flex flex-col">
+              <div className="-m-1.5 overflow-x-auto">
+                <div className="p-1.5 min-w-full inline-block align-middle">
+                  <div className="overflow-hidden">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="uppercase text-neutral-100 bg-slate-400 border-b-2 border-gray-500">
+                        <tr>
+                          {['Name', 'Email', 'Role', 'Action'].map((header) => (
+                            <th key={header} scope="col" className="px-6 py-3 text-center text-xs font-medium">
+                              {header}
+                            </th>
+                          ))}
+                        </tr>
+                      </thead>
+                      <tbody className="divide-y divide-gray-200">
+                        {userList.users.map((user, index) => (
+                          <tr key={index} className="bg-neutral-100">
+                            <td className="text-start px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-800">{user.name}</td>
+                            <td className="text-start px-4 py-2 whitespace-nowrap text-sm text-gray-600">{user.email}</td>
+                            <td className="text-start px-4 py-2 whitespace-nowrap text-sm text-gray-600">
+                              {user.roles?.map((r) => r.role).join(', ')}
+                            </td>
+                            <td className="px-4 py-2 whitespace-nowrap text-end text-sm font-medium">
+                              <button
+                                type="button"
+                                data-testid="delete-user"
+                                className="px-2 py-1 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-1 border-orange-400 text-orange-400 hover:border-orange-800 hover:text-orange-800"
+                                onClick={() => deleteUser(user)}
+                              >
+                                <TrashIcon />
+                                Delete
+                              </button>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <td className="px-1 py-1">
+                            <input
+                              type="text"
+                              ref={filterUserRef}
+                              name="filterUser"
+                              placeholder="Filter users"
+                              className="px-2 py-1 text-sm border border-gray-300 rounded-lg"
+                            />
+                            <button
+                              type="submit"
+                              className="ml-2 px-2 py-1 text-sm font-semibold rounded-lg border border-orange-400 text-orange-400 hover:border-orange-800 hover:text-orange-800"
+                              onClick={filterUsers}
+                            >
+                              Search
+                            </button>
+                          </td>
+                          <td colSpan={3} className="text-end text-sm font-medium">
+                            <button
+                              className="w-12 p-1 text-sm font-semibold rounded-lg border border-transparent bg-white text-grey border-grey m-1 hover:bg-orange-200 disabled:bg-neutral-300"
+                              onClick={() => setUserPage(userPage - 1)}
+                              disabled={userPage <= 0}
+                            >
+                              «
+                            </button>
+                            <button
+                              data-testid="users-next"
+                              className="w-12 p-1 text-sm font-semibold rounded-lg border border-transparent bg-white text-grey border-grey m-1 hover:bg-orange-200 disabled:bg-neutral-300"
+                              onClick={() => setUserPage(userPage + 1)}
+                              disabled={!userList.more}
+                            >
+                              »
+                            </button>
+                          </td>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        
       </View>
     );
   }
